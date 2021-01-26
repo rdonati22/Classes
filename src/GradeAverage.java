e9389d1f5e6a2cce37be7a790d0db057f492c1ad
rdonati22 <racheldonati@billericak12.com>
2021-01-26 15:21:22 -0500

    second commit

---
+++ src/GradeAverage.java
@@ -0,0 +1,79 @@
+/***
+ Consider a grading scheme where your final grade is computed more generously
+ if your grades show improvement over the term.  Scores show improvement
+ if each grade is greater than or equal to the previous grade.
+
+ A student has x scores indexed from 0 to x-1.  If the scores have shown improvement
+ only those grades with indexes greater than to equal to x/2 are averaged.  If the
+ scores have not improved, all of the scores are averaged.
+ ***/
+
+public class GradeAverage {
+
+    private int [] scores;
+
+    public GradeAverage(int [] s)
+    {
+        scores = s;
+    }
+
+    // returns the mean (average) of the values in the scores array
+    // whose indexes are between first and last (including first and last).
+    //You may assume that first and last are > 0 and < scores.length
+    public double mean(int first, int last)
+    {
+        int total = 0;
+        for (int i = first; i <= last; i++){
+            total += scores[i];
+        }
+        return (double) total / (last-first+1);
+    }
+
+    // returns true if each successive value in scores is greater than
+    // or equal to the previous value.  Otherwise returns false
+    public boolean showsImprovement()
+    {
+        int len = scores.length;
+        if (scores[len-1] < scores[len-2]){
+            return false;
+        }
+        for (int i = 0; i < len-1; i++){
+            if (scores[i] > scores[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // if the values in the scores array show improvement, returns the
+    // average of the elements in scores with indexes greater than or
+    // equal to scores.length()/2
+    public double finalGrade()
+    {
+        if (!showsImprovement()) {
+            return mean(0, scores.length-1);
+        }
+        else {
+            return mean(scores.length/2 , scores.length-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] s1 = {50,50,20,80,53};   // not improved, finalGrade is 50.6
+        int [] s2 = {20,50,50,53,80};   // improved, final grade is 61.0
+        int [] s3 = {20,50,50,85};      // improved, final grade is 67.5
+
+        GradeAverage sr1 = new GradeAverage(s1);
+        System.out.println(sr1.mean(1, 3));
+        System.out.println(sr1.showsImprovement());
+        System.out.println(sr1.finalGrade());
+        GradeAverage sr2 = new GradeAverage(s2);
+        System.out.println(sr2.mean(0, 4));
+        System.out.println(sr2.showsImprovement());
+        System.out.println(sr2.finalGrade());
+        GradeAverage sr3 = new GradeAverage(s3);
+        System.out.println(sr3.mean(0, 1));
+        System.out.println(sr3.showsImprovement());
+        System.out.println(sr3.finalGrade());
+    }
+}
---
+++ src/NumberDivider.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+/***
+ A positive integer is called a number divider if every decimal digit of
+ the number is a divisor of the number.  That is, if the number is evenly
+ divisible by each one of its digits.  For example, 128 is a number divider
+ because it is evenly divisible by 1, 2 and 8.  However, 26 is not a number
+ divider because if is not evenly divisible by 6.  Note that 0 is not
+ considered to be a divisor of any number, so any number containing a 0
+ digit is NOT a number divider.
+ ***/
+public class NumberDivider {
+    /** @param number the number to be tested
+     *		Precondition: number >0
+     * 	@return true if every decimal digit of number divides evenly
+     *		into the number, false otherwise
+     */
+    public static boolean isNumberDivider(int number)
+    {
+        for (int i = number; i > 0; i/=10){
+            if (i%10 == 0){
+                return false;
+            }
+            if (i%(i%10) != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** @param start starting point for values to be checker
+     *		Precondition: start >0
+     *	@param x the size of the array to be returned
+     *		Precondition: x > 0
+     *	@return an array containing the first num integers >= start
+     *		that are number dividers
+     **/
+//    public static int [] firstXNumberDividers(int start, int x)
+//    {
+//        int [] a = new int [x];
+//        int index = 0;
+//        for (int i = start; i < 150; i++){
+//            if (isNumberDivider(i)){
+//                a[index] = i;
+//                index++;
+//            }
+//        }
+//        return a;
+//    }
+
+    public static int [] firstXNumberDividers(int start, int x)
+    {
+        int [] a = new int [x];
+        int index = 0;
+        for (int i = start; index < x; i++){
+            if (isNumberDivider(i)){
+                a[index] = i;
+                index++;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 128, b = 124, c= 26, d = 101;
+        System.out.println(isNumberDivider(a));
+        System.out.println(isNumberDivider(b));
+        System.out.println(isNumberDivider(c));
+        System.out.println(isNumberDivider(d));
+
+        int [] divArr = firstXNumberDividers(128, 4);
+        System.out.println(Arrays.toString(divArr));
+    }
+}
